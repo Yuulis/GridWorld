af4fb03084c9a6fe48efe57ae0e9ff63b35b1c4c
Yuulis <yuulis2005@gmail.com>
2022-10-07 14:07:45 +0900

    プレイヤー、障害物の概念を追加

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ const CELL_SIZE = 8;
 // カラーリスト
 const COLOR_LIST = [
     '#f5f5f5',  // null(0)
-    '#00ff7f',  // green(1)
-    '#ff4545',  // red(2)
+    '#00ff7f',  // player(1)
+    '#d2691e',  // obstacle(2)
     '#00bfff',  // blue(3)
 ];
 
@@ -18,6 +18,10 @@ let cells_data = new Array();
 let canvas;
 let ctx;
 
+// プレイヤー
+let pos_x;
+let pos_y;
+
 
 window.onload = function ()
 {
@@ -26,6 +30,8 @@ window.onload = function ()
 
     width = Math.floor(canvas.width / CELL_SIZE);
     height = Math.floor(canvas.height / CELL_SIZE);
+
+    canvas.addEventListener('click', onClick, false);
 
     initialize();
 };
@@ -41,6 +47,10 @@ function initialize() {
             cells_data[y][x] = 0;
         }
     }
+
+    pos_x = Math.floor(Math.random() * width);
+    pos_y = Math.floor(Math.random() * height);
+    cells_data[pos_y][pos_x] = 1;
 
     ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -63,4 +73,46 @@ function reDraw() {
 function drawCell(x, y) {
     ctx.fillStyle = COLOR_LIST[cells_data[y][x]];
     ctx.fillRect(x * CELL_SIZE + 1, y * CELL_SIZE + 1, CELL_SIZE - 1, CELL_SIZE - 1);
+}
+
+
+// キー入力
+document.addEventListener('keydown', function (e) {
+    cells_data[pos_y][pos_x] = 0;
+
+    if (e.code == 'ArrowUp' && pos_y > 0 && cells_data[pos_y - 1][pos_x] != 2) {
+        pos_y--;
+    }
+    else if (e.code == 'ArrowDown' && pos_y < height - 1 && cells_data[pos_y + 1][pos_x] != 2) {
+        pos_y++;
+    }
+    else if (e.code == 'ArrowLeft' && pos_x > 0 && cells_data[pos_y][pos_x - 1] != 2) {
+        pos_x--;
+    }
+    else if (e.code == 'ArrowRight' && pos_x < width - 1 && cells_data[pos_y][pos_x + 1] != 2) {
+        pos_x++;
+    }
+
+    cells_data[pos_y][pos_x] = 1;
+    reDraw();
+
+	return false;
+});
+
+
+// フィールドがクリックされたとき
+function onClick(e) {
+    let pos_x = e.clientX - canvas.offsetLeft;
+    let pos_y = e.clientY - canvas.offsetTop;
+    let idx_x = Math.floor(pos_x / CELL_SIZE);
+    let idx_y = Math.floor(pos_y / CELL_SIZE);
+
+    if (cells_data[idx_y][idx_x] == 0) {
+        cells_data[idx_y][idx_x] = 2;
+    }
+    else if (cells_data[idx_y][idx_x] == 2) {
+        cells_data[idx_y][idx_x] = 0;
+    }
+
+    drawCell(idx_x, idx_y);
 }
