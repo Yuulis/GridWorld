1c69d868806b4d34c9428ff5f60970968aa72c97
Yuulis <yuulis2005@gmail.com>
2024-03-13 21:02:45 +0900

    Changed so that SetTiles() updates fieldTilemap by referencing CellData in fieldData.

--- Assets/Scripts/TilemapController.cs
+++ Assets/Scripts/TilemapController.cs
@@ -5,12 +5,13 @@ using UnityEngine.Tilemaps;
 
 public class TilemapController : MonoBehaviour
 {
-    [SerializeField] private Tilemap fieldTlemap;
+    [SerializeField] private Tilemap fieldTilemap;
     [SerializeField] private List<Tile> fieldTiles;
 
     public GameObject CSVReaderObj;
     private CSVReader CSVReader;
-    private List<List<int>> fieldData;
+
+    public List<List<CellData>> fieldData;
 
     void Start()
     {
@@ -31,7 +32,7 @@ public class TilemapController : MonoBehaviour
         {
             for (int x = 0; x < fieldData[y].Count; x++)
             {
-                fieldTlemap.SetTile(new Vector3Int(x, height - y - 1, 0), fieldTiles[fieldData[y][x]]);
+                fieldTilemap.SetTile(new Vector3Int(x, height - y - 1, 0), fieldTiles[fieldData[y][x].cellType]);
             }
         }
     }
